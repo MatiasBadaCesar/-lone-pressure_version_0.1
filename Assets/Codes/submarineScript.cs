af90cfefca98a6008a4d193d906225603119ba9a
harrylae <harry_lae@hotmail.com>
2020-08-24 20:55:08 -0300

    Joystick y Submarino 3D - Asset Agregado
    Se agrega Submarino y la jugabilidad del mismo. El Joystick es Dinámico.

---
+++ Assets/Codes/submarineScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class submarineScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speedMove = 1f;
+
+    //Creamos el Joystick
+    public Joystick joystickYAxes;
+    Transform tr;
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Si el usuario mueve el joystick verticalmente
+        tr.position = tr.position + new Vector3(0f,joystickYAxes.Vertical * speedMove * 1,0f); 
+        
+    }
+
+    
+}
