38b268c743a4a3a864b255d7dab0faf5e5a00a34
harrylae <harry_lae@hotmail.com>
2020-08-27 18:47:49 -0300

    Esqueleto Jugabilidad y Animaciones
    Se plantea un modelo para darle la jugabilidad requerida. El mismo será en formato de máquina de estados.

---
+++ Assets/Codes/juggernautScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class juggernautScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+
+    //Tomamos el transform para correr el Jugger hasta que comience el ataque
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(levelMgrScript.EstadosJuegoManager == levelMgrScript.EstadosJuego.Idle )Debug.Log("Estoy en Idle");
+        if(levelMgrScript.EstadosJuegoManager == levelMgrScript.EstadosJuego.JuggerIngresa )Debug.Log("Estoy en Ingresa");
+    }
+}
---
+++ Assets/Codes/levelMgrScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelMgrScript : MonoBehaviour
+{
+    private GameObject submarine;
+    public float velMovSubmarine = 0;
+    private float maxXmoveSub = 24; 
+
+    private GameObject jugger;
+
+    private float velScreens;
+    private float TiempoTranscurrido;
+    private float TiempoMaxJuggerIn = 10f;
+
+
+//Voy a definir los estados del juego como si fuera una Máquina de Estados
+public enum EstadosJuego {
+Entrada, //El juego comienza
+Idle, //El submarino esta en StandBy
+JuggerIngresa, //El Jugger ingresa a la scena
+JuggerAtaca1, //El JuggerAtaca de forma 1
+JuggerAtaca2, //El JuggerAtaca de forma 2
+JuggerSale, //El jugger se va
+SubPierdeVida, //El submarino pierde una vida
+SubMuere //El submarino muere para siempre nunca jamás
+}
+
+public static EstadosJuego EstadosJuegoManager;
+
+    void Start()
+    {
+        //Instanciamos al Submarino
+        submarine = GameObject.FindWithTag("Submarino");
+        //Instanciamos al Jugger
+        jugger = GameObject.FindWithTag("Jugger");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (EstadosJuegoManager)
+        {
+            case EstadosJuego.Entrada:
+                //Ubicamos al submarino en el centro de la scena
+                if(submarine.transform.position.x < maxXmoveSub)
+                {       
+                    submarine.transform.position = submarine.transform.position + new Vector3(velMovSubmarine,0f,0f); 
+                }
+                else
+                {
+                    TiempoTranscurrido = Time.time;
+                    EstadosJuegoManager = EstadosJuego.Idle;
+                }      
+
+            break;
+
+            case EstadosJuego.Idle:
+                BuscarTiempoParaJugger();
+            break;
+        }
+    }
+
+    private void BuscarTiempoParaJugger()
+    {
+        
+        if(Time.time - TiempoTranscurrido > TiempoMaxJuggerIn )
+        {
+            EstadosJuegoManager = EstadosJuego.JuggerIngresa;
+        }
+    }
+}
