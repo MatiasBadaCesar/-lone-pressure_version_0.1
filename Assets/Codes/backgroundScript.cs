c9fb22a24b3ab8c2c7724f912d05fab318681d5b
harrylae <harry_lae@hotmail.com>
2020-09-16 20:26:20 -0300

    Fondo rocas agregado y scrip.
    Texturas Jugger y Fondo

---
+++ Assets/Codes/backgroundScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundScript : MonoBehaviour
+{
+    public GameObject rockWall_1;
+    public GameObject rockWall_2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rockWall_1.GetComponent<Transform>().position = rockWall_1.GetComponent<Transform>().position + new Vector3(-0.2f,0f,0f);  
+        rockWall_2.GetComponent<Transform>().position = rockWall_2.GetComponent<Transform>().position + new Vector3(-0.2f,0f,0f); 
+    }
+}
