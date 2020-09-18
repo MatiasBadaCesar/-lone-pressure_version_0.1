b34d6e687095a887715f548e8a86e11919842812
harry_lae <harry_lae@hotmail.com>
2020-09-17 23:39:28 -0300

    Nueva versión
    Se pasa a la nueva versión 2020.1.5f1

--- Assets/Codes/backgroundScript.cs
+++ Assets/Codes/backgroundScript.cs
@@ -6,6 +6,7 @@ public class backgroundScript : MonoBehaviour
 {
     public GameObject rockWall_1;
     public GameObject rockWall_2;
+    public GameObject rockPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,18 @@ public class backgroundScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rockWall_1.GetComponent<Transform>().position = rockWall_1.GetComponent<Transform>().position + new Vector3(-0.2f,0f,0f);  
-        rockWall_2.GetComponent<Transform>().position = rockWall_2.GetComponent<Transform>().position + new Vector3(-0.2f,0f,0f); 
+        //Movemos el fondo hacia la izquierda
+        rockWall_1.transform.position = rockWall_1.transform.position + new Vector3(-0.2f,0f,0f);  
+        rockWall_2.transform.position = rockWall_2.transform.position + new Vector3(-0.2f,0f,0f); 
+
+        //A cierta posición en X cambiamos de GameObject destruimos uno y creamos uno nuevo para
+        //que se vea como si el fondo fuera continuo...
+        if(rockWall_2.transform.position.x < 20.0f)
+        { 
+            Object.Destroy(rockWall_1); //Destruimos el muro que salió de la pantalla
+            rockWall_1 = rockWall_2; //Cambiamos las instancias
+            rockWall_2 = Object.Instantiate(rockPrefab, new Vector3(58f,0.63f,6.2f), rockPrefab.transform.rotation); //Creamos un muro nuevo
+        }
+    
     }
 }
