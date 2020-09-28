c4cf561b5f480e73c9b41d3364fb3a292998f4c2
Matias Badaloni <53199112+MatiasBadaloni@users.noreply.github.com>
2020-09-28 16:32:25 -0300

    Menu
    Hector el menú esta hecho de manera simplona y desordenada. Sus archivos están dentro de la carpeta "Scenes", y allí estará la carpeta "Emergencia". El menú es un camvas así que debes remplazar el escenario Nivel para utilizarlo. Ademas, lo configure para que acceda a un menú principal (Por lo que debes configurar la valor publica de LevelManual, que esta disponible en el botón "Quit").

---
+++ Scenes/Emergencia/Pausa/PauseMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public void Pause()
+    {
+        Time.timeScale =0f;
+
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+    }
+
+}
